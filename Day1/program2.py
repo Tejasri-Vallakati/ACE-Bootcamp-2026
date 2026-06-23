d4b253d2e2045afe4f408c2a4e7c9a46f3d9e5ee
Tejasri-Vallakati <mail2tejasri.vallakati@gmail.com>
2026-06-23 09:57:19 +0000

    program2.py

--- Day1/program2.py
+++ Day1/program2.py
@@ -1,3 +1,9 @@
-first=int(input("enter one:"))
-second=int(input("enter two:"))
-print(first+second)
+
+def addition(first,second):
+    return first + second
+
+if __name__ == '__main__':
+    print("Running program2.py directly as a standalone script...")
+    
+    result = addition(5, 7)
+    print(f"Test Result: {result}")
