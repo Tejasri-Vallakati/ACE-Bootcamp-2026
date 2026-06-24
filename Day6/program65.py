3b26fd2fc1952eee0159f055ddc417d43776c76f
Tejasri-Vallakati <mail2tejasri.vallakati@gmail.com>
2026-06-24 08:20:05 +0000

    All Day6 programs

---
+++ Day6/program65.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr=np.array([2,4,5,8,9])
+print("Multiplication :",arr*2)#multiplication
+print("Addition: ",arr+5)#addition
+print("Power: ",arr**2)#power
+print("Subtraction: ",arr-1)#subtraction
+print("Division: ",arr/2)#division
+print("Modulus: ",arr%3)#modulus
+print(np.add(2,arr))
+print(np.subtract(1,arr))
+print(np.multiply(2,arr))
+print(np.divide(2,arr))
---
+++ Day6/program68.py
@@ -0,0 +1,6 @@
+import numpy as np
+marks=np.array([
+    [95,100,1,55,68],
+    [25,45,63,77,68]
+])
+print("Comparision : ",marks[0:1,marks > 45])
