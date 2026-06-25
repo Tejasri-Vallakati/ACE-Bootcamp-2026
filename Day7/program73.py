a45265dff73925554b4128e7f8726f39c88cda24
Tejasri-Vallakati <mail2tejasri.vallakati@gmail.com>
2026-06-25 09:25:07 +0000

    Remove archive folder from repo

--- Day7/program73.py
+++ Day7/program73.py
@@ -1,9 +1,13 @@
 import pandas as pd
-fn="/workspaces/ACE-Bootcamp-2026/Day7/archive/WorldCupMatches.csv"
+fn="/workspaces/ACE-Bootcamp-2026/Day7/archive (1)/used_car_price_prediction_1M.csv"
 df=pd.read_csv(fn)
 print(df.head(10))
 print(df.shape)
 print(df.tail(10))
 print(df.info())
-df.iloc[1]
+print(df.iloc[1])
 print(df.head())
+print(df.loc[0,'Model'])
+print(df.loc[:,'Brand']=='BMW')
+filt=(df['Brand']=='BMW')
+df.loc[filt].sort_index(ascending=False).head()
